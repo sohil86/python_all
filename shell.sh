4a9fc1ad72fc094a16fe586c7407e5aa78cf8d73
devajparikh <devajnparikh@gmail.com>
2017-09-29 13:57:50 +0530

    Updated the shell script to skip repeatitive build process

--- shell.sh
+++ shell.sh
@@ -1,15 +1,19 @@
+cd random/build
+make
+cd ..
+
 for number in `seq 1 100`
 do
 echo TEST NUMBER :- $number
-cd random/build
-make
-cd ../bin
+
+cd bin
 ./randnum
 exit_status=$?
 echo $exit_status
-cd ../build
-make clean
+#cd ../build
+#make clean
 cd ..
 cd ..
 python prime.py $exit_status
+cd random
 done
